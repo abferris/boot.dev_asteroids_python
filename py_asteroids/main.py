fe6d9251da8d9e76b7d0b083fc31d83e07acaea0
abferris <abferris@gmail.com>
2025-08-27 14:14:37 -0700

    i made the shifting gradient into background functions that are changable and repeatable Base of the background is still black, and the gradient only goes from black to one other color

--- py_asteroids/main.py
+++ py_asteroids/main.py
@@ -1,6 +1,8 @@
 import pygame
 
 from src.abstractions.constants import *
+from src.abstractions.background import *
+
 def main():
     pygame.init()
     dims = SCREEN_WIDTH, SCREEN_HEIGHT
@@ -16,32 +18,18 @@ def main():
 
 # background with fill function plain black
         black = (0, 0, 0)
-        screen.fill(black)
+        # screen.fill(black)
+        set_solid_background(black, screen)
+        
 
-# background with draw line function for a blue to black gradient
-        # for y in range(SCREEN_HEIGHT):
-        #     b_cval = int(127 * (y / SCREEN_HEIGHT))
-        #     blue = (0, 0, b_cval)
-        #     pygame.draw.line(screen, blue, (0, y), (SCREEN_WIDTH, y))
-
-# shifting background with gradient
-        for y in range(SCREEN_HEIGHT):
-            grad_height = SCREEN_HEIGHT
-            half_grad_height = SCREEN_HEIGHT / 2
-
-            grad_pos = (y + bg_offset) % grad_height
-
-            if grad_pos < grad_height/2 :
-                b_cval = int(127 * (grad_pos / (half_grad_height)))
-            else :
-                b_cval = int(127 * (1 - (grad_pos - half_grad_height) / (half_grad_height)))
-            blue = (0, 0, b_cval)
-            pygame.draw.line(screen, blue, (0, y), (SCREEN_WIDTH, y))
+        bg_secondary = (60,0,0)
+        set_shifting_gradient(bg_secondary, screen,dims,bg_offset)
 
 #  refreshes screen 
-        pygame.display.flip()
+        bg_offset += .6  # increase for faster movement
 
-        bg_offset += 1  # increase for faster movement
-        clock.tick(40)  # limit to 60 FPS
+        run_loop(90,clock)
+        # clock.tick(90)  # limit to 60 FPS
+        # pygame.display.flip()
 if __name__ == "__main__":
     main()
